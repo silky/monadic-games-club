6b98675750a16c68db1f009c63118c84bf06979b
Noon van der Silk <noonsilk+-noonsilk@gmail.com>
2025-02-19 09:46:53 +0000

    hacking

--- src/Games/Skull.hs
+++ src/Games/Skull.hs
@@ -2,13 +2,9 @@
 {-# language TemplateHaskell       #-}
 {-# language UndecidableInstances  #-}
 
--- https://downloads.haskell.org/ghc/latest/docs/users_guide/using-warnings.html#ghc-flag--Wmissing-deriving-strategies
 {-# OPTIONS_GHC -Wno-missing-deriving-strategies #-}
--- https://downloads.haskell.org/ghc/latest/docs/users_guide/using-warnings.html#ghc-flag--Wunrecognised-pragmas
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
--- https://downloads.haskell.org/ghc/latest/docs/users_guide/using-warnings.html#ghc-flag--Wunticked-promoted-constructors
 {-# OPTIONS_GHC -Wno-unticked-promoted-constructors #-}
--- https://downloads.haskell.org/ghc/latest/docs/users_guide/using-warnings.html#ghc-flag--Wunused-type-patterns
 {-# OPTIONS_GHC -Wno-unused-type-patterns #-}
 
 module Games.Skull where
@@ -24,6 +20,9 @@ module Games.Skull where
   They win if they do that; or lose if they don't.
 -}
 
+import "base" Control.Monad (when)
+import "base" Prelude hiding (init, reverse, id)
+import "containers" Data.Map (Map)
 import "crem" Crem.BaseMachine (InitialState (..))
 import "crem" Crem.Decider (Decider (..), EvolutionResult (..))
 import "crem" Crem.Render.RenderableVertices (AllVertices (..), RenderableVertices)
@@ -44,6 +43,10 @@ data Card
   | Skull  Suit
   deriving stock (Eq, Show)
 
+data PlayerStack = PlayerStack
+  { cards :: [Card]
+  }
+
 suit :: Card -> Suit
 suit (Flower s) = s
 suit (Skull s)  = s
@@ -52,20 +55,21 @@ newtype PlayerId = PlayerId Int
   deriving newtype (Eq, Show, Num)
 
 newtype PlayerCount = PlayerCount Int
-  deriving newtype (Eq, Show, Num)
+  deriving newtype (Eq, Show, Num, Ord)
 
 data PlayerState = PlayerState
-  { playerId :: PlayerId
+  { id :: PlayerId
   , playerCount :: PlayerCount
   }
+  deriving stock (Show)
 
 init :: PlayerId -> PlayerCount -> PlayerState
 init playerId count = PlayerState playerId count
 
-next :: PlayerState -> PlayerState
-next PlayerState{playerId = PlayerId i, playerCount = PlayerCount count}
+nextPlayer :: PlayerState -> PlayerState
+nextPlayer PlayerState{id = PlayerId i, playerCount = PlayerCount count}
   = PlayerState
-      { playerId = PlayerId $ i + 1 `mod` count
+      { id = PlayerId $ i + 1 `mod` count
       , playerCount = PlayerCount count
       }
 
@@ -84,8 +88,7 @@ data PlayData = PlayData
   deriving stock (Eq, Show)
 
 data BetData = BetData
-  { playerId :: PlayerId
-  , flowers :: Int
+  { flowers :: Int
   }
   deriving stock (Eq, Show)
 
@@ -116,3 +119,64 @@ $( singletons
  )
 
 deriving via AllVertices SkullsVertex instance RenderableVertices SkullsVertex
+
+
+-- * State
+
+data StateData = StateData
+  { bet :: BetData
+  , currentPlayer :: PlayData
+  , playerStacks :: Map PlayerId PlayerStack
+  }
+
+data SkullsState (vertex :: SkullsVertex) where
+  SkullsInitialState :: SkullsState 'Initial
+  SkullsStartedState :: StateData -> SkullsState 'Started
+
+data GameError
+  = TooFewPlayers
+  | GameAlreadyStarted
+  | GameNotStarted
+  | NotYourTurn
+  deriving stock (Eq, Show)
+
+-- * Machine
+
+decider
+  :: InitialState SkullsState
+  -> Decider SkullsTopology Command (Either GameError Event)
+decider initialState =
+  Decider
+    { deciderInitialState = initialState
+    , decide = decide
+    , evolve = evolve
+    }
+  where
+    decide command state
+      = case (state, command) of
+          --
+          -- ** Starting a game
+          (SkullsStartedState _, StartGame _) ->
+            Left GameAlreadyStarted
+
+          (_, StartGame initialData)
+            | players initialData < PlayerCount 2 -> Left TooFewPlayers
+            | otherwise ->
+              Right $
+                GameStarted
+                  initialData
+                  ( id $
+                      nextPlayer
+                        (init (PlayerId 0) $ players initialData)
+                  )
+
+          --
+          -- ** Playing a card
+          (SkullsInitialState, PlayCard _) ->
+            Left GameNotStarted
+
+          (SkullsStartedState stateData, PlayCard playData)
+            | playerId (currentPlayer stateData) /= playerId playData -> Left NotYourTurn
+            | otherwise -> Right $ CardPlayed playData
+
+    evolve = undefined
