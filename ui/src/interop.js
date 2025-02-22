1cf4365a76cac7836a239c6852e01e128dbb54b4
Noon van der Silk <noonsilk+-noonsilk@gmail.com>
2025-02-22 23:12:08 +0000

    Less comments

--- ui/src/interop.js
+++ ui/src/interop.js
@@ -1,18 +1,12 @@
 const s = new WebSocket("ws://localhost:8008/game");
 
-s.addEventListener("open", (event) => {
-  console.log("Connected!");
-});
-
 s.addEventListener("message", (event) => {
   console.log("Received: ", event.data);
   app.ports.receiveMessage.send(event.data);
 });
 
 export const onReady = ({ app, env }) => {
-  console.log("Ready!");
   if (app.ports) {
-    console.log("Subscribed!");
     app.ports.sendMessage.subscribe((data) => {
       console.log("Sending: ", JSON.stringify(data));
       s.send(JSON.stringify(data));
